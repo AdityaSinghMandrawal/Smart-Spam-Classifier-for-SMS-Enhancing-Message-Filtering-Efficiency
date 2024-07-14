00248d2951d4f0ad7158cfe83af76226d1273db8
Aditya Singh Mandrawal <169059776+AdityaSinghMandrawal@users.noreply.github.com>
2024-07-14 18:06:33 +0530

    Add files via upload

--- sms_spam_classifier.py
+++ sms_spam_classifier.py
@@ -6,6 +6,11 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix, accuracy_score
+import nltk
+
+# Download NLTK resources (if not already downloaded)
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 # Load the dataset
 messages = pd.read_csv(r'C:\Users\Acer\Documents\SMS spam classifier\SMSSpamCollection', sep='\t',
@@ -18,7 +23,7 @@ for i in range(0, len(messages)):
     review = messages['message'][i]
     review = review.lower()
     review = review.split()
-    review = [w.lemmatize(word) for word in review if not word in stopwords.words('english')]
+    review = [w.lemmatize(word) for word in review if not word in set(stopwords.words('english'))]
     review = ' '.join(review)
     corpus.append(review)
 
@@ -27,8 +32,7 @@ cv = CountVectorizer(max_features=1000)
 X = cv.fit_transform(corpus).toarray()
 
 # Encoding the labels
-y = pd.get_dummies(messages['label'])
-y = y.iloc[:, 1].values
+y = messages['label'].map({'ham': 0, 'spam': 1}).values
 
 # Train Test Split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
@@ -46,4 +50,4 @@ accuracy = accuracy_score(y_test, y_pred)
 # Displaying results
 print("Confusion Matrix:")
 print(confusion_m)
-print("Accuracy:", accuracy)
+print("\nAccuracy:", accuracy)
